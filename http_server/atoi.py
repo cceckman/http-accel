c2daf1c3dea958dc2c45b0300c5978f8f972acfb
Stephen Longfield <stephen.longfield@gmail.com>
2025-02-13 23:38:20 -0800

    Flesh out atoi module

--- http_server/atoi.py
+++ http_server/atoi.py
@@ -1,9 +1,8 @@
 import amaranth as am
 
-from amaranth import Module
+from amaranth import Module, Signal, unsigned, Const
 from amaranth.lib.wiring import In, Out, Component
 from amaranth.lib import stream
-
 
 class AtoI(Component):
     """
@@ -26,13 +25,36 @@ class AtoI(Component):
 
     def __init__(self, width: int, **kwargs):
         super().__init__({
-                "input" : In(stream.Signature(8)),
+                "input" : In(stream.Signature(unsigned(8))),
                 "reset" : In(1),
-                "error" : Out(1),
-                "value" : Out(width),
+                "error" : Out(1, init=0),
+                "value" : Out(width, init=0),
             },  **kwargs)
+        self._width = width
 
-    def elaborate(self, platform):
+    def elaborate(self, _platform):
         m = Module()
+
+        next = Signal(self._width)
+        shifted = Signal(self._width)
+        increment = Signal(self._width)
+        error_comb = Signal(1)
+
+        # Ready to get data if we're out of reset.
+        m.d.comb += self.input.ready.eq(~self.reset)
+
+        m.d.comb += [
+            error_comb.eq(self.input.valid & 
+                          (self.input.payload < Const(48)) | (self.input.payload > Const(57))),
+            # x*10 = x*8+x*2 = x<<3+x<<1
+            shifted.eq((self.value << 3) + (self.value << 1)),
+            increment.eq(self.input.payload - Const(48))
+        ]
+
+        m.d.comb += next.eq(am.Mux(self.input.valid, shifted + increment, self.value))
+
+        # Error latches, and holds until next reset
+        m.d.sync += self.error.eq(am.Mux(self.reset, 0, self.error | error_comb))
+        m.d.sync += self.value.eq(am.Mux(error_comb | self.reset, 0, next))
 
         return m
--- http_server/atoi_test.py
+++ http_server/atoi_test.py
@@ -4,11 +4,59 @@ from atoi import AtoI
 
 dut = AtoI(32)
 
-async def test_exhaustive(ctx):
-    pass
+async def run_test_case(ctx, input: str, expected: int) -> int: 
+    ctx.set(dut.reset, 1)
+    await ctx.tick()
+    ctx.set(dut.reset, 0)
+
+    for c in input:
+        ctx.set(dut.input.payload, ord(c))
+        ctx.set(dut.input.valid, 1)
+        await ctx.tick()
+        ctx.set(dut.input.valid, 0)
+        assert ctx.get(dut.error) == 0
+    
+    assert ctx.get(dut.value) == expected
+
+
+async def test_simple(ctx):
+    await run_test_case(ctx, "1", 1)
+    await run_test_case(ctx, "12", 12)
+    await run_test_case(ctx, "123", 123)
+    await run_test_case(ctx, "1234", 1234)
+    await run_test_case(ctx, "12345", 12345)
+
+async def test_simple_error(ctx):
+    ctx.set(dut.reset, 1)
+    await ctx.tick()
+    ctx.set(dut.reset, 0)
+    await ctx.tick()
+    assert ctx.get(dut.error) == 0
+
+    ctx.set(dut.input.payload, ord("A"))
+    ctx.set(dut.input.valid, 1)
+    await ctx.tick()
+    assert ctx.get(dut.error) == 1
+
+    # Error bit is sticky
+    ctx.set(dut.input.payload, ord("0"))
+    await ctx.tick()
+    assert ctx.get(dut.error) == 1
+
+    # Reset clears it
+    ctx.set(dut.reset, 1)
+    await ctx.tick()
+    ctx.set(dut.reset, 0)
+    await ctx.tick()
+    assert ctx.get(dut.error) == 0
+
+async def bench(ctx):
+    await test_simple(ctx)
+    await test_simple_error(ctx)
 
 sim = Simulator(dut)
-sim.add_testbench(test_exhaustive)
+sim.add_clock(1e-6)
+sim.add_testbench(bench)
 
 if __name__ == "__main__":
     import sys
