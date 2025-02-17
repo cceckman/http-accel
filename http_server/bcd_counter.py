61161ee4b11b19e3da583503241e9727362b7779
Stephen Longfield <stephen.longfield@gmail.com>
2025-02-16 23:09:07 -0800

    Add+test BCD counter.

--- http_server/bcd_counter.py
+++ http_server/bcd_counter.py
@@ -1,4 +1,4 @@
-from amaranth import Module, Signal, unsigned 
+from amaranth import Module, Signal, unsigned, Array, Const
 from amaranth.lib.wiring import In, Out, Component
 from amaranth.lib import stream
 
@@ -50,6 +50,9 @@ class BcdCounter(Component):
     By counting in BCD, it is able to efficiently print the output as a decimal
     number. Outputs can either be numeric, or ASCII-encoded.
 
+    Note that if counting is enabled after output has been triggered, the results
+    may be strange.
+    
     Parameters
     ----------
     digits : int
@@ -59,17 +62,17 @@ class BcdCounter(Component):
 
     Attributes
     ----------
-    reset    : Signal(1), in. Resets the counter.
-    en       : Signal(1), in
-               The counter is incremented on each cycle where `en` is asserted,
-               otherwise retains its value.
-    triggger : Signal(1), in
-               One-shot trigger, start writing the message to output.
-    ovf      : Signal(1), out
-               `ovf` is asserted when the counter overflows
-    output   : Stream(8), out
-               The data stream to write the message to.
-    done     : High when stream is inactive, i.e., writing is done.
+    reset   : Signal(1), in. Resets the counter.
+    en      : Signal(1), in
+              The counter is incremented on each cycle where `en` is asserted,
+              otherwise retains its value.
+    trigger : Signal(1), in
+              One-shot trigger, start writing the message to output.
+    ovf     : Signal(1), out
+              `ovf` is asserted when the counter overflows
+    outpu   : Stream(8), out
+              The data stream to write the message to.
+    done    : High when stream is inactive, i.e., writing is done.
     """
 
     def __init__(self, width, ascii):
@@ -85,7 +88,47 @@ class BcdCounter(Component):
         self._width = width
         self._ascii = ascii
 
+
     def elaborate(self, unused_platform):
         m = Module()
+
+        digits = Array()
+        for d in range(self._width):
+            m.submodules[f"digit_{d}"] = BcdDigit()
+            m.submodules[f"digit_{d}"].reset = self.reset
+            digits.append(m.submodules[f"digit_{d}"].digit)
+        m.submodules.digit_0.en = self.en
+
+        for d in range(1, self._width):
+            m.submodules[f"digit_{d}"].en = m.submodules[f"digit_{d-1}"].ovf
+        self.ovf = m.submodules[f"digit_{self._width-1}"].ovf
+
+        # This really just needs to be ceil(log_2(10^width)) bits wide, but 4*width 
+        # should be good enough.
+        count = Signal(4*self._width)
+
+        with m.FSM():
+            with m.State("idle"):
+                m.d.comb += [
+                    self.done.eq(Const(1)),
+                ]
+                m.d.sync += count.eq(Const(self._width-1))
+                with m.If(self.trigger):
+                    m.next = "print"
+                with m.Else():
+                    m.next = "idle"
+            with m.State("print"):
+                m.d.comb += [
+                    self.output.payload.eq(
+                        digits[count]+48 if self._ascii else digits[count]
+                    ),
+                    self.output.valid.eq(Const(1)),
+                    self.done.eq(Const(0))
+                ]
+                with m.If(self.output.ready):
+                    with m.If(count > 0):
+                        m.d.sync += count.eq(count-1)
+                    with m.Else():
+                        m.next = "idle"
 
         return m
