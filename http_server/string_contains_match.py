5fcb76d934c228eb55b76b436fd8ee91021143d0
Stephen Longfield <stephen.longfield@gmail.com>
2025-03-16 14:05:17 -0700

    Add a stream matcher that matcheds .*{message}
    Plan is to use this to match the "/r/n/r/n" that indicates the end
    of the headers in HTTP/1.0.

---
+++ http_server/string_contains_match.py
@@ -0,0 +1,96 @@
+from amaranth import Module, Signal, Array
+from amaranth.lib.wiring import In, Out, Component
+from amaranth.lib import stream
+from capitalizer import Capitalizer
+
+
+class StringContainsMatch(Component):
+    """
+    Matches a literal string in a stream.
+
+    Looks to see if the stream contains the substring, even if it doesn't start
+    with it. Equivalent to the regex ".*{message}"
+
+    Returns "accepted" upon match.
+
+    Parameters
+    ----------
+    message:    str
+                String to match.
+    match_case: bool, default true
+                Perform a case-sensitive match.
+                Case folding is ASCII-only.
+
+    Attributes
+    ----------
+    input:      Stream(8), in
+                Data stream to match.
+    accepted:   Signal(1), out
+                High if the string has been matched.
+    reset:      Signal(1), in
+                Reset and await new input.
+    """
+
+    input: In(stream.Signature(8))
+    accepted: Out(1)
+    reset: In(1)
+
+    def __init__(self, message: str, match_case: bool = True, **kwargs):
+        super().__init__(**kwargs)
+
+        cased_message = message
+        if not match_case:
+            cased_message = message.upper()
+        self._message = Array(map(ord, cased_message))
+
+        self._match_case = match_case
+
+
+    def elaborate(self, _platform):
+        m = Module()
+
+        # Case-normalized data:
+        c = Signal(8)
+        if self._match_case:
+            m.d.comb += c.eq(self.input.payload)
+        else:
+            m.submodules.capitalizer = capitalizer = Capitalizer()
+            m.d.comb += [
+                    capitalizer.input.eq(self.input.payload),
+                    c.eq(capitalizer.output),
+            ]
+
+        import math
+        size = math.ceil(math.log2(len(self._message))) + 1
+        idx = Signal(size)
+
+        with m.FSM():
+            with m.State("matching"):
+                m.d.comb += [
+                    self.input.ready.eq(1),
+                    self.accepted.eq(0),
+                ]
+                m.next = "matching"
+                with m.If(self.input.valid & ~self.reset):
+                    # Consume one byte; we're already holding "ready".
+                    with m.If(c == self._message[idx]):
+                        with m.If(idx < len(self._message) - 1):
+                            # Accept this character and move on.
+                            m.d.sync += idx.eq(idx + 1)
+                        with m.Else():
+                            # At the end of the string.
+                            m.next = "accepted"
+                    with m.Else():
+                        # Restart search
+                        m.d.sync += idx.eq(0)
+            with m.State("accepted"):
+                m.next = "accepted"
+                m.d.comb += [
+                    self.accepted.eq(1), 
+                    self.input.ready.eq(0)
+                ]
+                m.d.sync += idx.eq(0)
+                with m.If(self.reset):
+                    m.next = "matching"
+
+        return m
---
+++ http_server/string_contains_match_test.py
@@ -0,0 +1,55 @@
+import sys
+
+from amaranth.sim import Simulator
+from amaranth.lib.wiring import connect
+
+from string_contains_match import StringContainsMatch
+from stream_fixtures import StreamSender
+
+def test_finds_substring():
+    dut = StringContainsMatch("test")
+    sender = StreamSender(stream=dut.input)
+    sim = Simulator(dut)
+    sim.add_clock(1e-6)
+
+    async def driver(ctx):
+        ctx.set(dut.reset, 1)
+        ctx.tick()
+        ctx.set(dut.reset, 0)
+
+        while not sender.done:
+            await ctx.tick()
+        assert ctx.get(dut.accepted)
+
+    sim.add_testbench(driver)
+    sim.add_process(sender.send_passive(map(ord, "This is a test of string contains")))
+
+    with sim.write_vcd(sys.stdout):
+        sim.run_until(0.0001)
+
+def test_no_match():
+    dut = StringContainsMatch("test")
+    sender = StreamSender(stream=dut.input)
+    sim = Simulator(dut)
+    sim.add_clock(1e-6)
+
+    async def driver(ctx):
+        ctx.set(dut.reset, 1)
+        ctx.tick()
+        ctx.set(dut.reset, 0)
+
+        while not sender.done:
+            await ctx.tick()
+        assert ctx.get(dut.accepted)==0
+
+    sim.add_testbench(driver)
+    sim.add_process(sender.send_passive(map(ord, "This is a trial of string contains")))
+
+    with sim.write_vcd(sys.stdout):
+        sim.run_until(0.0001)
+
+
+
+if __name__ == "__main__":
+    test_finds_substring()
+    test_no_match()
