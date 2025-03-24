b4554dba4f7e9c17f9dbeb3a741fc828ed616e69
Charles Eckman <charles@cceckman.com>
2025-03-23 20:26:19 -0400

    Continue porting to pytest

--- http_server/string_match_test.py
+++ http_server/string_match_test.py
@@ -3,75 +3,138 @@ from amaranth.sim import Simulator
 
 from string_match import StringMatch
 
-dut = StringMatch("Hello", match_case=True)
 
+def test_match_sensitive():
 
-async def run_sequence(ctx, input: str):
-    """
-    Runs the input sequence into the DUT until accepted, rejected,
-    or indeterminate after the input has been passsed.
+    dut = StringMatch("Hello", match_case=True)
 
-    Returns true iff the input was accepted;
-    fails assertion if the input length was exceeded.
-    """
-    assert ctx.get(dut.accepted) == 0
-    assert ctx.get(dut.rejected) == 0
+    async def run_sequence(ctx, input: str):
+        """
+        Runs the input sequence into the DUT until accepted, rejected,
+        or indeterminate after the input has been passsed.
 
-    input = input + "     "
-    count = 0
+        Returns true iff the input was accepted;
+        fails assertion if the input length was exceeded.
+        """
+        assert ctx.get(dut.accepted) == 0
+        assert ctx.get(dut.rejected) == 0
 
-    while True:
-        if ctx.get(dut.accepted) or ctx.get(dut.rejected):
-            break
-        if count > len(input):
-            break
-        assert ctx.get(dut.input.ready)
-        valid = random.randint(0, 1)
-        ctx.set(dut.input.valid, valid)
-        if valid:
-            # Advance the input on this cycle.
-            count += 1
-            ctx.set(dut.input.payload, ord(input[0]))
-            input = input[1:]
+        input = input + "     "
+        count = 0
+
+        while True:
+            if ctx.get(dut.accepted) or ctx.get(dut.rejected):
+                break
+            if count > len(input):
+                break
+            assert ctx.get(dut.input.ready)
+            valid = random.randint(0, 1)
+            ctx.set(dut.input.valid, valid)
+            if valid:
+                # Advance the input on this cycle.
+                count += 1
+                ctx.set(dut.input.payload, ord(input[0]))
+                input = input[1:]
+            await ctx.tick()
+
+        # After matching-or-not, this is no longer "ready" for input.
+        assert not ctx.get(dut.input.ready)
+        assert ctx.get(dut.accepted) or ctx.get(dut.rejected)
+        accepted = ctx.get(dut.accepted)
         await ctx.tick()
 
-    # After matching-or-not, this is no longer "ready" for input.
-    assert not ctx.get(dut.input.ready)
-    assert ctx.get(dut.accepted) or ctx.get(dut.rejected)
-    accepted = ctx.get(dut.accepted)
-    await ctx.tick()
+        return accepted
 
-    return accepted
+    async def bench(ctx):
+        matches_hello = await run_sequence(ctx, "Hello")
+        assert matches_hello
+
+        ctx.set(dut.reset, 1)
+        await ctx.tick()
+        ctx.set(dut.reset, 0)
+        ctx.set(dut.input.valid, 0)
+
+        matches_goodbye = await run_sequence(ctx, "Goodbye")
+        assert not matches_goodbye
+
+        ctx.set(dut.reset, 1)
+        await ctx.tick()
+        ctx.set(dut.reset, 0)
+        ctx.set(dut.input.valid, 0)
+
+        matches_hello_lower = await run_sequence(ctx, "hello")
+        assert not matches_hello_lower
+
+    sim = Simulator(dut)
+    sim.add_clock(1e-6)
+    sim.add_testbench(bench)
+
+    sim.run()
 
 
-async def bench(ctx):
-    matches_hello = await run_sequence(ctx, "Hello")
-    assert matches_hello
+def test_match_insensitive():
 
-    ctx.set(dut.reset, 1)
-    await ctx.tick()
-    ctx.set(dut.reset, 0)
-    ctx.set(dut.input.valid, 0)
+    dut = StringMatch("Hello", match_case=False)
 
-    matches_goodbye = await run_sequence(ctx, "Goodbye")
-    assert not matches_goodbye
+    async def run_sequence(ctx, input: str):
+        """
+        Runs the input sequence into the DUT until accepted, rejected,
+        or indeterminate after the input has been passsed.
 
-    ctx.set(dut.reset, 1)
-    await ctx.tick()
-    ctx.set(dut.reset, 0)
-    ctx.set(dut.input.valid, 0)
+        Returns true iff the input was accepted;
+        fails assertion if the input length was exceeded.
+        """
+        assert ctx.get(dut.accepted) == 0
+        assert ctx.get(dut.rejected) == 0
 
-    matches_hello_lower = await run_sequence(ctx, "hello")
-    assert not matches_hello_lower
+        input = input + "     "
+        count = 0
 
+        while True:
+            if ctx.get(dut.accepted) or ctx.get(dut.rejected):
+                break
+            if count > len(input):
+                break
+            assert ctx.get(dut.input.ready)
+            valid = random.randint(0, 1)
+            ctx.set(dut.input.valid, valid)
+            if valid:
+                # Advance the input on this cycle.
+                count += 1
+                ctx.set(dut.input.payload, ord(input[0]))
+                input = input[1:]
+            await ctx.tick()
 
-sim = Simulator(dut)
-sim.add_clock(1e-6)
-sim.add_testbench(bench)
+        # After matching-or-not, this is no longer "ready" for input.
+        assert not ctx.get(dut.input.ready)
+        assert ctx.get(dut.accepted) or ctx.get(dut.rejected)
+        accepted = ctx.get(dut.accepted)
+        await ctx.tick()
 
-# Doesn't appear to be a way to _remove_ a testbench;
-# I guess .reset() is "just" to allow a different initial state?
-if __name__ == "__main__":
-    import sys
-    with sim.write_vcd(sys.stdout):
-        sim.run()
+        return accepted
+
+    async def bench(ctx):
+        matches_hello = await run_sequence(ctx, "Hello")
+        assert matches_hello
+
+        ctx.set(dut.reset, 1)
+        await ctx.tick()
+        ctx.set(dut.reset, 0)
+        ctx.set(dut.input.valid, 0)
+
+        matches_hello = await run_sequence(ctx, "hello")
+        assert matches_hello
+
+        ctx.set(dut.reset, 1)
+        await ctx.tick()
+        ctx.set(dut.reset, 0)
+        ctx.set(dut.input.valid, 0)
+
+        matches_goodbye = await run_sequence(ctx, "goodbye")
+        assert not matches_goodbye
+
+    sim = Simulator(dut)
+    sim.add_clock(1e-6)
+    sim.add_testbench(bench)
+
+    sim.run()
