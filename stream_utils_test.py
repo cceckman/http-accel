318b6bb185447cee6bf995122e1e023176147ae0
Charles Eckman <charles@cceckman.com>
2025-03-23 20:33:05 -0400

    Remove output from stream_utils_test.py

--- stream_utils_test.py
+++ stream_utils_test.py
@@ -47,9 +47,5 @@ def test_limit_forward():
     sim.add_testbench(driver)
     sim.add_clock(1e-6)
 
-    with sim.write_vcd(sys.stdout):
-        sim.run()
+    sim.run()
 
-
-if __name__ == "__main__":
-    test_limit_forward()
