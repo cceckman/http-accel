8f140110eeb5f8c80a27d868f1dd2d4065d00785
Charles Eckman <charles@cceckman.com>
2025-03-23 20:52:26 -0400

    Simulation fixture: run a serial device with a host program

---
+++ sim_server.py
@@ -0,0 +1,115 @@
+import queue
+import sys
+from threading import Thread
+
+from amaranth.sim import Simulator
+
+from stream_fixtures import StreamSender, StreamCollector
+
+
+class SimServer:
+    """
+    A holder for an Amaranth simulation of serial-connected component.
+
+    This wraps the provided dut in a simulator,
+    and provides access to the TX and RX lines via another thread.
+
+    Use as a context manager:
+
+    ```
+    with SimServer(dut, dut.tx, dut.rx) as sim:
+        sim.send([0, 1, 2, 3])
+        v = sim.recv()
+    ```
+    """
+
+    def __init__(self, dut, dut_tx, dut_rx):
+        """
+        Create a server.
+
+        Arguments:
+        dut:            An Amaranth component.
+        dut_tx:         The device-under-test's TX line (outbound from device).
+        dut_rx:         The device-under-test's RX line (inbound to device).
+        """
+
+        self._dut = dut
+        self._dut_tx = dut_tx
+        self._dut_rx = dut_rx
+        self._data_in = None
+        self._data_out = None
+        self._sender = None
+        self._sim_thread = None
+
+    def send(self, b: bytes):
+        """
+        Transmit the provided bytes to the simulation.
+        """
+        assert self._sim_thread is not None, (
+            "Simulation is not running; enter the context")
+        self._data_in.put(b)
+
+    def recv(self, count=None):
+        """
+        Receive bytes from the simulation.
+
+        If count is set, receive at least `count` bytes (possibly more).
+        Buffering is your problem!
+        """
+        assert self._sim_thread is not None, (
+            "Simulation is not running; enter the context")
+        if count is None:
+            return self._data_on.get()
+        else:
+            buffer = bytes()
+            while len(buffer) < count:
+                buffer += self._data_out.get()
+            return buffer
+
+    def __enter__(self):
+        """
+        Start the simulation in a background thread.
+        """
+
+        assert self._sim_thread is None
+        sim = Simulator(self._dut)
+        sim.add_clock(1e-6)
+
+        self._data_in = queue.Queue()
+        self._data_out = queue.Queue()
+
+        tx = self._sender = StreamSender(self._dut_rx)
+        rx = StreamCollector(self._dut_tx)
+
+        sim.add_process(rx.collect_queue(self._data_out))
+        sim.add_testbench(tx.send_queue_active(self._data_in))
+
+        # Start a new thread for simulation.
+        # The Amaranth simulator provides its own async context,
+        # but the .run() method is not itself async, so we have to use threads.
+        self._sim_thread = Thread(target=self._run_sim(sim))
+        self._sim_thread.start()
+        return self
+
+    def _run_sim(self, sim):
+        def runnable():
+            try:
+                print("running simulator")
+                sim.run()
+                print("simulation complete")
+            except Exception as e:
+                sys.stderr.write(f"error in Amaranth simulation: {e}")
+                # Try to force shutdown:
+                self._sender.done = True
+
+        return runnable
+
+    def __exit__(self, *args, **kwargs):
+        assert self._sim_thread is not None
+        # Shutting down the data input should shut down the simulator;
+        # the data input is driving the tick.
+        # self._data_in.shutdown()
+        # .shutdown() is not available on python3.11,
+        # so we have to use a flag.
+        self._sender.done = True
+        self._sim_thread.join()
---
+++ sim_server_test.py
@@ -0,0 +1,49 @@
+
+from capitalizer import Capitalizer
+from amaranth import Module
+from amaranth.lib.wiring import Component, In, connect
+from amaranth.lib import stream
+from amaranth.lib import fifo
+from sim_server import SimServer
+
+
+class DelayCapitalizer(Component):
+    """
+    A capitalizer but on a one-cycle delay.
+    """
+
+    rx: In(stream.Signature(8))
+    # This should be Out...but runs into the cases where wiring.connect
+    # mistakes In and Out for submodules.
+    tx: In(stream.Signature(8))
+
+    def elaborate(self, platform):
+        m = Module()
+        caps = m.submodules.caps = Capitalizer()
+        queue = m.submodules.fifo = fifo.SyncFIFOBuffered(width=8, depth=1)
+
+        m.d.comb += [
+            caps.input.eq(self.rx.payload),
+            queue.w_stream.payload.eq(caps.output),
+            queue.w_stream.valid.eq(self.rx.valid),
+            self.rx.ready.eq(queue.w_stream.ready),
+        ]
+        connect(m, queue.r_stream, self.tx)
+        return m
+
+
+def test_sim():
+    dut = DelayCapitalizer()
+
+    with SimServer(dut, dut.tx, dut.rx) as srv:
+        hello = "hello, world"
+        print("sending data")
+        srv.send(hello)
+        print("sent data, awaiting response")
+
+        got = srv.recv(len(hello))
+        print("got response")
+        want = b"HELLO, WORLD"
+        assert got == want, (
+            f"unexpected result: got \"{got}\", want \"{want}\""
+        )
